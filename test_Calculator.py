e78e22ccd22eb6cac2cfec37c400a84cefc00157
Nathan Budiman <jonathan@radialogica.com>
2020-05-28 10:15:32 -0500

    All basic functions (Working)

--- test_Calculator.py
+++ test_Calculator.py
@@ -3,9 +3,22 @@ from Calculator import Calculator
 
 class MyTest (unittest.TestCase):
     def test_add(self):
-        calcObject = Calculator(2,5)
+        calcObject = Calculator(5,5)
         x = calcObject.add()
-        self.assertEqual(7, x)
+        self.assertEqual(10, x)
+
+    def test_subtract(self):
+        calcObject = Calculator(5,5)
+        a = calcObject.subtract()
+        self.assertEqual(0, a)
+    def test_multiply(self):
+        calcObject = Calculator(5,5)
+        b = calcObject.multiply()
+        self.assertEqual(25, b)
+    def test_divide(self):
+        calcObject = Calculator(5,5)
+        c = calcObject.divide()
+        self.assertEqual(1, c)
 
 if __name__ == '__main__':
     unittest.main()
