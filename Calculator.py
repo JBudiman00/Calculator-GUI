cf471b8c9db44c3fff674d1d5445186f4fef28cb
Nathan Budiman <jonathan@radialogica.com>
2020-05-28 11:59:34 -0500

    Fully functioning Calculator

--- Calculator.py
+++ Calculator.py
@@ -25,3 +25,36 @@ class Calculator:
     
     def log10 (self):
         return math.log10(self.value1)
+
+def main():
+    while (True):
+        option = input("Enter the operation you would like to do\n1. Add \n2. Subtract \n3. Multiply \n4. Divide \n5. Exponentiate \n6. log10\n")
+        value1 = input("Enter the first value\n")
+        if option != "6":
+            value2 = input("Enter the second value\n")
+
+        option = int(option)
+        value1 = int(value1)
+        value2 = int(value2)
+
+        obj = Calculator(value1, value2)
+
+        if option == 1:
+            print(obj.add())
+        elif option == 2:
+            print(obj.subtract())
+        elif option == 3:
+            print(obj.multiply())
+        elif option == 4:
+            print(obj.divide())
+        elif option ==5:
+            print(obj.Exponentiate())
+        else:
+            print(obj.log10())
+
+        choice = input("Run again? (Y/N)\n")
+
+        if choice == "N":
+            break
+
+main()
